4d5ce345cbe114f42741c4d615f4481e466054f8
jaab30 <jaab30@yahoo.com>
2020-02-17 15:30:18 -0500

    Updates

---
+++ precache-manifest.5e9ce44fbf59a358b9e8ae48169d501d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd819005b6f6d337a3a472439096aea4",
+    "url": "/employee-management/index.html"
+  },
+  {
+    "revision": "1ab85f0c0f963d9737b4",
+    "url": "/employee-management/static/css/main.58758951.chunk.css"
+  },
+  {
+    "revision": "fae560e56509405f1248",
+    "url": "/employee-management/static/js/2.6b4270d5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/employee-management/static/js/2.6b4270d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ab85f0c0f963d9737b4",
+    "url": "/employee-management/static/js/main.7679c9a8.chunk.js"
+  },
+  {
+    "revision": "466de7e0521e29d401c2",
+    "url": "/employee-management/static/js/runtime-main.06b777f5.js"
+  }
+]);
